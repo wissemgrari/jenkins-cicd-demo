ce503320e7ed2dce4bdaf589824ae170d855559a
Wissem <wissemgrari@outlook.com>
2026-01-07 11:06:38 +0100

    Add Dockerfile and update .dockerignore

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+db.init()
+    .then(() => {
+        app.listen(3000, () => console.log('Listening on port 3000'));
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
 
 const gracefulShutdown = () => {
     db.teardown()
